5fe3a49c6e84305af3a9786c9a3f7a2e3ad57a84
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-12 17:15:54 +0200

    added pair possibility

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -5,12 +5,13 @@ function getRndInteger(min, max) {
 // catturo i bottoni con una variabile
 const btnGenerate = document.querySelector(".btn-primary")
 const btnReset = document.querySelector(".btn-secondary")
-// creo i div dove inserire mosse, vittoria/sconfitta
+// creo i div dove inserire mosse, vittoria/sconfitta/pareggio
 const extContainer = document.querySelector(".container")
 const userPlay = document.createElement("div");
 const pcPlay = document.createElement("div");
 const victory = document.createElement("div");
 const loss = document.createElement("div");
+const pair = document.createElement("div");
 
 btnGenerate.addEventListener("click", function() {
     // faccio generare due numeri casuali tramite button
@@ -38,9 +39,10 @@ btnGenerate.addEventListener("click", function() {
         `
         extContainer.append(victory);
         loss.remove();
+        pair.remove();
     } 
     // se il numero dell'utente è minore del pc allora stampo "sconfitta"
-    else {
+    else if(userNum < pcNum){
         loss.innerHTML = 
         `
         <div>Hai perso...</div>
@@ -48,6 +50,18 @@ btnGenerate.addEventListener("click", function() {
         `
         extContainer.append(loss);
         victory.remove();
+        pair.remove();
+    }
+    // se il numero dell'utente è pari del pc allora stampo "pareggio"
+    else if(userNum === pcNum){
+        pair.innerHTML = 
+        `
+        <div>Pareggio!</div>
+        <span>Riprova</span>
+        `
+        extContainer.append(pair);
+        victory.remove();
+        loss.remove();
     }
 }
 )
